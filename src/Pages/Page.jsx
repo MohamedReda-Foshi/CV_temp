a5fb0a79a6b40329daa5e6787e6d3474dca66d2b
Mohamed Reda Foshi <reda.foshi11@gmail.com>
2024-05-20 17:00:52 +0100

    fix the nav bar problem

--- src/Pages/Page.jsx
+++ src/Pages/Page.jsx
@@ -5,73 +5,55 @@ import Portfolio from "../componentes/Portfolio";
 
 import { useState } from "react";
 
-
-
-
 import { Link } from "react-scroll";
 import { BrowserRouter } from "react-router-dom";
 
 export default function Page() {
-
   const [Copoment, setCopoment] = useState("Home");
 
   const Page_Home = () => {
-    setCopoment('Home');
+    setCopoment("Home");
   };
 
   const Page_About = () => {
-    setCopoment('About');
+    setCopoment("About");
   };
   const Page_Portfolio = () => {
-    setCopoment('Portfolio');
+    setCopoment("Portfolio");
   };
 
-  
-  
-  
-
-
- 
   return (
-    <div className="grid grid-cols-5">
-       <div>
-        <div className=" fixed h-dvh w-full justify-center mx-12 hidden md:block">
-          <div className="py-80">
+
+
+  <div className="grid grid-cols-4">
+
+
+      <div className="  ">
+
+        <div className="   hidden md:block fixed">
+
+          <div className="py-80 h-lvh bg-stone-300 col-span-1 px-11 ">
+
+
+
             <div>
               <h1 className="text-4xl font-bold text-wrap uppercase font-bolds">
                 title full name
               </h1>
             </div>
+
+
+
             <div className="my-9 ">
               <BrowserRouter>
                 <ul className="flex flex-col justify-items-center ">
                   <li className=" py-1  ">
-                  
                     <Link
                       spy={true}
                       smooth={true}
                       className="relative cursor-pointer text-xl w-fit block after:block after:content-after:absolute after:h-[3px] after:bg-black dark:after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
                     >
-                    <button onClick={Page_Home}>
-                    Home
-                    </button>
-                      
-                    </Link>
-             
-                  </li>
-                  <li className="py-1">
- 
-                    <Link
-                    
-                      spy={true}
-                      smooth={true}
-                      className="relative cursor-pointer text-xl w-fit block after:block after:content-after:absolute after:h-[3px] after:bg-black dark:after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
-                    >
-
-                      <button onClick={Page_About}>
-                        About
-                    </button>
-           
+                      <button onClick={Page_Home}>Home</button>
                     </Link>
                   </li>
                   <li className="py-1">
@@ -80,67 +62,65 @@ export default function Page() {
                       smooth={true}
                       className="relative cursor-pointer text-xl w-fit block after:block after:content-after:absolute after:h-[3px] after:bg-black dark:after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
                     >
-                    
-                    <button onClick={Page_Portfolio}>
-                        Portfolio
-                    </button>
-
-                      
+                      <button onClick={Page_About}>About</button>
+                    </Link>
+                  </li>
+                  <li className="py-1">
+                    <Link
+                      spy={true}
+                      smooth={true}
+                      className="relative cursor-pointer text-xl w-fit block after:block after:content-after:absolute after:h-[3px] after:bg-black dark:after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
+                    >
+                      <button onClick={Page_Portfolio}>Portfolio</button>
                     </Link>
                   </li>
                   <li className="py-1">
                     <Link className="relative cursor-pointer text-xl w-fit block after:block after:content-after:absolute after:h-[3px] after:bg-black dark:after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">
-                      
                       Contact
                     </Link>
                   </li>
                 </ul>
               </BrowserRouter>
             </div>
-            <div>
-              <p className="font-light">
-                <p>202? Morocco</p>
-                <p>Created by Mohamed Reda Foshi</p>
-              </p>
+                                  <div>
+                                    <p className="font-light">
+                                      <p>202? Morocco</p>
+                                      <p>Created by Mohamed Reda Foshi</p>
+                                    </p>
+                                  </div>
             </div>
-          </div>
+            
         </div>
-      </div> 
-
-      <div className="col-span-4 h-lvh">
-
-             
-                
-      
-                <div>
-        {Copoment === 'Home' && (
-          <div
- >
-              <Home/>
-          </div>
-        )}
-
-
-        {Copoment === 'About' && (
-          <div
-  >
-            <About/>
-          </div>
-        )}
-
-        {Copoment === 'Portfolio' && (
-          <div
- >
-            <Portfolio/>
-          </div>
-        )}
-
 
 
       </div>
-              
-          
+
+
+
+
+
+      <div className="  col-span-3 ">
+        <div>
+          {Copoment === "Home" && (
+            <div>
+              <Home />
+            </div>
+          )}
+
+          {Copoment === "About" && (
+            <div>
+              <About />
+            </div>
+          )}
+
+          {Copoment === "Portfolio" && (
+            <div>
+              <Portfolio />
+            </div>
+          )}
+        </div>
       </div>
+
     </div>
   );
 }
--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -8,10 +8,10 @@ import { FaInstagram } from "react-icons/fa6";
 function Home() {
   return (
     <>
-      <div id="Home" className="h-lvh bg-rose-100 px-28 flex items-center flex-row text-base/loose  ">
+      <div id="Home" className="h-lvh px-28 flex items-center flex-row text-base/loose  ">
         <div>
           <div className="px-5">
-            <img className=" rounded-full  lg:w-80 lg:h-80   " src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
+            <img className=" rounded-full  lg:w-72 lg:h-52   " src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
           </div>
         </div>
 
@@ -34,7 +34,7 @@ function Home() {
             </div>
 
             <div>
-              <ul className="flex flex-row justify-around py-3 text-base/loose">
+              <ul className="flex flex-row justify-around py-3 px-52 text-base/loose">
                 <li>
                   <AiFillFacebook size={30} />
                 </li>
